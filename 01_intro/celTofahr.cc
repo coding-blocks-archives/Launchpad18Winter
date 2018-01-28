2635cd6565a7ca457b8dbda5003bf4c79c4a445b
Deepak Aggarwal <daggarwal77@gmail.com>
2018-01-28 14:04:02 +0530

    Intro

---
+++ 01_intro/celTofahr.cc
@@ -0,0 +1,27 @@
+// Deepak Aggarwal, Coding Blocks
+// deepak@codingblocks.com
+#include <iostream>
+using namespace std;
+int main(){
+    int start, final, step;
+    cin >> start >> final >> step;
+
+    // while(start <= final){
+    //     // do something
+    //     double cel = (5.0 / 9.0) * (start - 32);
+    //     cout << start << " F" << " " << cel << " C" << endl;
+    //     start = start + step;
+    // }   
+    // cout << start;
+
+    // for(int fahr = start; fahr <= final; fahr = fahr + step){
+    //     double cel = (5.0 / 9.0) * (fahr - 32);
+    //     cout << fahr << " F" << " " << cel << " C" << endl;
+    // }
+
+
+    //  for(int curNum = 1; curNum <= 10; curNum = curNum + 1){
+    //     cout << curNum << endl;
+    // }
+
+}
