93b761e61c3cbaf8e22b67d91fd07e5aa3ef342c
Deepak Aggarwal <daggarwal77@gmail.com>
2018-02-10 13:23:11 +0530

    cntChar

---
+++ 04_functions/cntChar.cc
@@ -0,0 +1,14 @@
+// Deepak Aggarwal, Coding Blocks
+// deepak@codingblocks.com
+#include <iostream>
+using namespace std;
+int main(){
+    char c;
+    // cin >> c;
+    c = cin.get();
+    cout << "X" << c << "X";   
+    c = cin.get();
+    cout << "X" << c << "X";   
+    c = cin.get();
+    cout << "X" << c << "X";   
+}
