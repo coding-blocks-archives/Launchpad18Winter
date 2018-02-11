dd0c56ff6b7740c1df7e15b4270a5e2ad068b92b
Deepak Aggarwal <daggarwal77@gmail.com>
2018-02-11 10:29:59 +0530

    Count Words

--- 04_functions/cntCharDesc.cc
+++ 04_functions/cntCharDesc.cc
@@ -3,6 +3,25 @@
 #include <iostream>
 using namespace std;
 
+bool isAlphabet(char c){
+    // if (c >= 'a' and c <= 'z') return true;
+    // if (c >= 'A' && c <= 'Z') return true;
+
+    // if ((c >= 'a' and c <= 'z') or (c >= 'A' && c <= 'Z')) return true;
+    // if ((c >= 'a' and c <= 'z') || (c >= 'A' && c <= 'Z')) return true;
+    // return false;
+
+    return (c >= 'a' and c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+bool isDigit(char c){
+    return c >= '0' && c <= '9';
+}
+
+bool isWhitespace(char c){
+    return c == ' ' or c == '\n' or c == '\t';
+}
+
 void printCnt(){
     int nWords = 0;
     int nLetter = 0;
@@ -12,25 +31,35 @@ void printCnt(){
     int nDigits = 0;
     int whiteSpaces = 0;
     int nAlpha = 0;
+    bool isInsideWord = false;
 
     while(true){
         char c = cin.get();
         if (c == '$'){
+            if (isInsideWord) ++nWords;
             break;
         }
 
-        ++letters;
+        ++nLetter;
         if (isAlphabet(c)){
             ++nAlpha;
+            isInsideWord = true;
         }
         else if (isDigit(c)){
             ++nDigits;
+            isInsideWord = true;
         }
         else if (isWhitespace(c)){
             ++whiteSpaces;
+            if (isInsideWord == true){
+                // now out of the word
+                ++nWords;
+            }
+            isInsideWord = false;
         }
         else {
             ++nOthers;
+            isInsideWord = true;
         }
     }
 
@@ -38,6 +67,7 @@ void printCnt(){
     cout << "digits\t:" << nDigits << endl;
     cout << "letters\t:" << nLetter << endl;
     cout << "spaces\t:" << nSpaces << endl;
+    cout << "whiteSpaces\t:" << whiteSpaces << endl;
     cout << "others\t:" << nOthers << endl;
     cout << "nLines\t:" << nLines << endl;
 }
