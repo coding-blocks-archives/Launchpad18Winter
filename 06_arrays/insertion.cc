1d9d674722bf2d51c67d3bfddb6eef1208b4e4b2
Deepak Aggarwal <daggarwal77@gmail.com>
2018-02-17 13:38:30 +0530

    binarySearch

--- 06_arrays/insertion.cc
+++ 06_arrays/insertion.cc
@@ -3,6 +3,23 @@
 #include <iostream>
 using namespace std;
 
+int binarySearch(int arr[], int be, int en, int elementToFind){
+    while(be <= en){
+        int mid = (be + en) / 2;
+        if (arr[mid] == elementToFind){
+            return mid;
+        }
+        else if (arr[mid] < elementToFind){
+            be = mid + 1;
+        }
+        else {
+            en = mid - 1;
+        }
+    }
+    return -1;
+}
+
+
 void insertionSort(int arr[], int n){
     for(int i = 1; i < n; ++i){
         int tmp = arr[i];
@@ -54,7 +71,12 @@ int main() {
     cin >> n;
     inputArr(arr, n);
     // insertionSort(arr, n);
-    selectionSort(arr, n);
-    outputArr(arr, n);
+    // selectionSort(arr, n);
+    
+    int x; cin >> x;
+    int ans = binarySearch(arr, 0, n - 1, x);
+    cout << "element found @ " << ans;
+
+    // outputArr(arr, n);
 
 }
