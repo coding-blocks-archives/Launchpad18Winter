11b59544445441fbc547034dc5639249ad8d25fb
Deepak Aggarwal <daggarwal77@gmail.com>
2018-03-04 11:31:17 +0530

    Inc Dec Printing

--- 09_recursion/fact.cc
+++ 09_recursion/fact.cc
@@ -23,6 +23,35 @@ int fib(int n){
     return ans;
 }
 
+void printInc(int n){
+    // that prints n integers in inc order and after 
+    // printing the number, prints a space also
+    // 1_2_3_4_5_
+
+    // Yr! what if n is 0? Can't say printInc(-1)
+    if (n <= 0) return ;
+
+    // 1_2_3_4_ recursion se
+    printInc(n -1); // same problem as original
+    // 5_
+    cout << n << " ";
+}
+
+void printInc1(int n){
+    if (n <= 0) return;
+    printInc1(n - 1);
+    cout << n << "_";
+}
+
+void printDec(int n){
+    if (n <= 0){
+        return;
+    }
+
+    cout << "_" << n;
+    printDec(n - 1);
+}
+
 
 int main(){
     int n;
@@ -31,6 +60,10 @@ int main(){
     // int ans = fact(n);
     // cout << ans;    
 
-    int ans = fib(n);
-    cout << ans;
+    // int ans = fib(n);
+    // cout << ans;
+
+    // printInc1(n);
+
+    printDec(n);
 }
