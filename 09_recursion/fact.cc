9acacbd13153b793c22d80fd5944d1d4d1e66edf
Deepak Aggarwal <daggarwal77@gmail.com>
2018-03-04 14:13:53 +0530

    print 007

--- 09_recursion/fact.cc
+++ 09_recursion/fact.cc
@@ -74,7 +74,7 @@ void dispArr(int arr[], int n) {
 }
 
 int maxElementArr(int arr[], int n) {
-    if (n == 0){
+    if (n == 0) {
         int inf = 1000000;
         return -inf;
     }
@@ -88,13 +88,13 @@ int maxElementArr(int arr[], int n) {
 }
 
 
-int searchArr(int arr[], int n, int x){
-    if (n <= 0){
+int searchArr(int arr[], int n, int x) {
+    if (n <= 0) {
         return -1;
     }
 
     int posInRemArr = searchArr(arr, n - 1, x);
-    if (posInRemArr != -1){
+    if (posInRemArr != -1) {
         // element has been found
         return posInRemArr;
     }
@@ -105,16 +105,89 @@ int searchArr(int arr[], int n, int x){
 }
 
 
-int searchArr(int arr[], int n, int x){
-    if (n <= 0){
+int searchArr1(int arr[], int n, int x) {
+    if (n <= 0) {
         return -1;
     }
 
     if (arr[n - 1] == x) return n - 1;
 
-    return searchArr(arr, n - 1, x);
+    return searchArr1(arr, n - 1, x);
 
 }
+
+void bubbleSort(int arr[], int startIndex, int endIndex) {
+    if (startIndex >= endIndex) {
+        return;
+    }
+
+    bubbleSort(arr, startIndex + 1, endIndex);
+    if (arr[startIndex] > arr[startIndex + 1]) {
+        swap(arr[startIndex], arr[startIndex + 1]);
+        bubbleSort(arr, startIndex + 1, endIndex);
+    }
+}
+
+bool isSorted(int arr[], int startIndex, int endIndex) {
+    if (startIndex >= endIndex) {
+        return true;
+    }
+
+    if (arr[startIndex] < arr[startIndex + 1]) {
+        return isSorted(arr, startIndex + 1, endIndex);
+    }
+    return false;
+}
+
+int sumArr(int arr[], int be, int en) {
+    if (be > en) {
+        return 0;
+    }
+
+    return arr[be] + sumArr(arr, be + 1, en);
+}
+
+int binarySearch(int arr[], int be, int en, int key){
+    if (be > en){
+        return -1;
+    }
+
+    int mid = (be + en) / 2;
+    if (arr[mid] == key){
+        return mid;
+    }
+
+    if (arr[mid] > key){
+        return binarySearch(arr, be, mid - 1, key);
+    }
+
+    return binarySearch(arr, mid + 1, en, key);
+}
+
+void printDig(int dig){
+    char table[][10] = {
+        "zero", "one", "two", 
+        "three", "four", "five", 
+        "six", "seven", "eight", "nine"
+    };
+    // 10 rows, 6 colums. Don't forget the null char, man!
+    cout << table[dig];
+}
+
+void printNum(int num){
+    if (num == 0){
+        return;
+    }
+
+    int smallNum = num / 10;
+    printNum(smallNum);
+
+    int unitDigit = num % 10;
+    printDig(unitDigit);
+    cout << " ";
+}
+
+
 
 int main() {
     // int n;
@@ -134,11 +207,27 @@ int main() {
     // int ans = power(n, x);
     // cout << ans;
 
-    int arr[10], n;
-    cin >> n;
-    for (int i = 0; i < n; ++i) cin >> arr[i];
+    // int arr[10], n;
+    // cin >> n;
+    // for (int i = 0; i < n; ++i) cin >> arr[i];
     // dispArr(arr, n);
 
-    int ans = maxElementArr(arr, n);
-    cout << ans;
+    // int ans = maxElementArr(arr, n);
+    // cout << ans;
+
+    // bubbleSort(arr, 0, n - 1);
+    // dispArr(arr, n);
+
+    // bool ans = isSorted(arr, 0, n - 1);
+    // cout << ans;
+
+    // int ans = sumArr(arr, 0, n - 1);
+    // cout << ans;
+
+    // int key; cin >> key;
+    // int pos = binarySearch(arr, 0, n - 1, key);
+    // cout << pos;
+
+    int num; cin >> num;
+    printNum(num);
 }
