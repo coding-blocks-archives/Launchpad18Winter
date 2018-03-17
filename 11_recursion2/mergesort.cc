4cc6647ecc5b60253d6fc4bd13e57a5bddc5915c
Deepak Aggarwal <daggarwal77@gmail.com>
2018-03-17 12:37:21 +0530

    Keypad

--- 11_recursion2/mergesort.cc
+++ 11_recursion2/mergesort.cc
@@ -1,6 +1,7 @@
 // Deepak Aggarwal, Coding Blocks
 // deepak@codingblocks.com
 #include <iostream>
+#include <cstring>
 using namespace std;
 void inputArr(int arr[], int n) {
     for (int i = 0; i < n; ++i) {
@@ -14,8 +15,51 @@ void outputArr(int arr[], int n) {
     cout << endl;
 }
 
-void mergeSort(int arr[], int be, int en){  //[be,en]
-    if (be >= en){
+void copy(int a[], int be, int en, int x[]){
+    while(be <= en){
+        x[be] = a[be];
+        ++be;
+    }
+}
+
+void mergeSortedArr(int a[], int be, int mid, int en) {
+    int x[100];
+    int y[100];
+    copy(a, be, mid, x);
+    copy(a, mid + 1, en, y);
+
+    int i = be;         // x
+    int j = mid + 1;    // y
+    int k = be;         // arr
+
+    while(i <= mid && j <= en){
+        if (x[i] < y[j]){
+            a[k] = x[i];
+            ++i;
+            ++k;
+        }
+        else {
+            a[k] = y[j];
+            k++;
+            ++j;
+        }
+    }
+
+    while(i <= mid){
+        a[k] = x[i];
+        ++i;
+        ++k;
+    }
+
+    while(j <= en){
+        a[k] = y[j];
+        ++j;
+        ++k;
+    }
+}
+
+void mergeSort(int arr[], int be, int en) { //[be,en]
+    if (be >= en) {
         return;
     }
 
@@ -25,14 +69,48 @@ void mergeSort(int arr[], int be, int en){  //[be,en]
     mergeSortedArr(arr, be, mid, en);
 }
 
+void getButton(char dig, char arr[]){
+    char table[][5] = {
+        " ", 
+        "?*$", "abc", "def",
+        "ghi", "jkl", "mno",
+        "pqrs", "tuv", "wxyz"
+    };
 
+    int digit = dig - '0';  // index
+    strcpy(arr, table[digit]);
+}
+
+void generateCodes(char inp[], int be, char ansSoFar[], int i){
+    if (inp[be] == '\0'){
+        ansSoFar[i] = '\0';
+        cout << ansSoFar << endl;
+        return;
+    }
+
+    char dig = inp[be];
+    char code[10] = {};
+    getButton(dig, code);
+
+    for(int j = 0; code[j] != '\0'; ++j){
+        ansSoFar[i] = code[j];
+        generateCodes(inp, be + 1, ansSoFar, i + 1);
+    }
+}
 
 
 int main() {
-    int n;
-    cin >> n;
-    int arr[100] = {};
-    inputArr(arr, n);
-    mergeSort(arr, 0, n-1);
-    outputArr(arr, n);
+    /*Merge Sort*/
+    // int n;
+    // cin >> n;
+    // int arr[100] = {};
+    // inputArr(arr, n);
+    // mergeSort(arr, 0, n - 1);
+    // outputArr(arr, n);
+
+    /*keypad problem*/
+    char inp[100], out[100];
+    cin >> inp;
+    generateCodes(inp, 0, out, 0);
+
 }
