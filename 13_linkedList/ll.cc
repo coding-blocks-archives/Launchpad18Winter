ad0f2b70aba05dc597d32a13758755e88e2a4b86
Deepak Aggarwal <daggarwal77@gmail.com>
2018-03-24 12:59:09 +0530

    Merge LL

--- 13_linkedList/ll.cc
+++ 13_linkedList/ll.cc
@@ -45,10 +45,89 @@ void display(Node* head){
     }
 }
 
+void deleteNode(Node* &head, int key){
+    Node* cur = head;
+    Node* prevNode = NULL;
+
+    while(cur){
+        if (cur->data == key){
+            if (prevNode){
+                prevNode->next = cur->next;
+                delete cur; // cur stores the address of the current node
+            }
+            else {
+                // previous node is not THERE ==> cur MUST be head
+                head = cur->next;
+                delete cur;
+            }
+            break;
+        }
+        else {
+            prevNode = cur;
+            cur = cur->next;
+        }
+    }
+}
+
+Node* reverseLL(Node* head){
+    if (head == NULL || head->next == NULL){
+        return head;
+    }
+    Node* smallHead = reverseLL(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return smallHead;
+}
+
+Node* reverseLL2(Node* head){
+    Node* cur = head;
+    Node* prevNode = NULL;
+
+    while(cur){
+        Node* ahead = cur->next;
+        cur->next = prevNode;
+        prevNode = cur;
+        cur = ahead;
+    }
+    return prevNode;
+}
+
+Node* mergeLL(Node* a, Node* b){
+    if (a == NULL) return b;
+    if (b == NULL) return a;
+
+    if (a->data < b->data){
+        a->next = mergeLL(a->next, b);
+        return a;
+    }
+    else {
+        b->next = mergeLL(a, b->next);
+        return b;
+    }
+}
 
 
 int main(){
-    Node* head = createLL();
-    display(head);
+    // Node* head = createLL();
+    // display(head);
+    // cout << "\n***********\n";
+    // deleteNode(head, 2);
     
+    // head = reverseLL2(head);
+    // display(head);
+
+
+    Node* a = createLL();
+    Node* b = createLL();
+    Node* c = mergeLL(a, b);
+    display(c);
+
+
 }
+
+
+
+// reverse
+// merge 2 sorted
+// add numbers
+// detect cycle
