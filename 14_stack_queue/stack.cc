d69f7bc03b88c9df8843d6049fe5947310dea6ec
Deepak Aggarwal <daggarwal77@gmail.com>
2018-03-25 14:06:03 +0530

    Stack Impl

--- 14_stack_queue/stack.cc
+++ 14_stack_queue/stack.cc
@@ -4,16 +4,66 @@
 using namespace std;
 
 class Stack{
-    // data members
+    class Node{
+public:
+    int data;
+    Node* next;  
+    // ctor1
+    Node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+    Node* head;
 
 public:
-    Stack(){}
-    bool empty(){}
-    int top(){}
-    void push(int x){}
-    void pop(){}
+    Stack(){
+        head = NULL;
+    }
+    bool empty(){
+        return head == NULL;
+    }
+    int top(){
+        if (empty()) return -1;
+        return head->data;
+    }
+    void push(int x){
+        Node* tmp = new Node(x);
+        tmp->next = head;
+        head = tmp;
+    }
+    void pop(){
+        if(!empty()){
+            Node* tmp = head;
+            head = head->next;
+            delete tmp;
+        }
+    }
+    ~Stack(){
+        cout << "Dtor called..\n";
+        Node* tmp = NULL;
+        while(head){
+            tmp = head->next;
+            delete head;
+            head = tmp;
+        }
+    }
 };
 
 int main(){
-    
+
+    Stack s;
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    cout << s.top()<< endl;
+    cout << s.top()<< endl;
+
+    s.pop();
+    s.pop();
+
+    cout << s.top()<< endl;
+
 }
