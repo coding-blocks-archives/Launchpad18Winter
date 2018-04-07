238d478a986a50bbb8f9be657dcb0301b84cda51
Deepak Aggarwal <daggarwal77@gmail.com>
2018-04-07 13:40:54 +0530

    TopView

--- 17_trees/main.cc
+++ 17_trees/main.cc
@@ -1,6 +1,8 @@
 #include "tree.h"
 #include <iostream>
 #include <queue>
+#include <map>
+#include <utility>
 using namespace std;
 
 int cntNodes(TreeNode* root) {
@@ -21,7 +23,7 @@ int sumNodes(TreeNode* root) {
     return q1 + q2 + root->data;
 }
 
-TreeNode* levelWiseInput(){
+TreeNode* levelWiseInput() {
     queue<TreeNode*> q;
     int x; cin >> x;
 
@@ -31,19 +33,19 @@ TreeNode* levelWiseInput(){
     auto root = new TreeNode(x);
     q.push(root);
 
-    while(q.empty() == false){
+    while (q.empty() == false) {
         TreeNode* curPar = q.front();
         q.pop();
 
         cin >> x;
-        if (x != -1){
+        if (x != -1) {
             // left child is there
             curPar->left = new TreeNode(x);
             q.push(curPar->left);
-        } 
+        }
 
         cin >> x;
-        if (x != -1){
+        if (x != -1) {
             curPar->right = new TreeNode(x);
             q.push(curPar->right);
         }
@@ -51,15 +53,84 @@ TreeNode* levelWiseInput(){
     return root;
 }
 
+void levelWiseOuptut(TreeNode* root) {
+    if (root == NULL) return;
+
+    TreeNode* const DELIMITER = NULL;
+
+    queue<TreeNode*> q;
+    q.push(root);
+    q.push(DELIMITER);
+
+    while (q.empty() == false) {
+        // nodes to process
+        TreeNode* curNode = q.front();
+        q.pop();
+
+        if (curNode == DELIMITER){
+            cout << endl;
+            if (q.empty() == false){
+                // nodes are still there to be processed
+                q.push(DELIMITER);
+            }
+            continue;
+        }
+        cout << curNode->data << " ";
+
+        if (curNode->left) q.push(curNode->left);
+        if (curNode->right)q.push(curNode->right);
+    }
+}
+
+void topView(TreeNode* root){
+    if (root == NULL) return;
+
+    typedef pair<int, TreeNode*> PAIR;
+
+    queue<PAIR> q;      // pair of node* int
+    map<int, TreeNode*> m;    // hd-->val
+
+    q.push(make_pair(0, root));
+
+    while(q.empty() == false){
+        PAIR p = q.front();
+        q.pop();
+        TreeNode* curNode = p.second;
+        int curDist = p.first;
+
+        if (m.find(curDist) == m.end()){
+            // this is the first node with horizontal dist curDist
+            m[curDist] = curNode;
+        }
+
+        if (curNode->left){
+            q.push(make_pair(curDist - 1, curNode->left));
+        }
+        if (curNode->right){
+            q.push(make_pair(curDist + 1, curNode->right));
+        }
+    }
+
+    typedef map<int, TreeNode*>::iterator mapIterator;   // iterator to the map
+
+    for(mapIterator it = m.begin(); it != m.end(); ++it){
+        cout << it->second->data << " ";  // second is the val
+    }
+    cout << endl;
+}
+
 int main() {
     TreeNode* root = levelWiseInput();
-    preOrderPrint(root);
-    cout << endl;
+    // preOrderPrint(root);
+    // cout << endl;
 
     // int ans = cntNodes(root);
     // print(ans);
 
-    int ans = sumNodes(root);
-    print(ans);
+    // int ans = sumNodes(root);
+    // print(ans);
+
+    levelWiseOuptut(root);
+    topView(root);
 
 }
